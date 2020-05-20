a73bf035274a50d417ba0d1ecf443dc1fdd0c7a1
Tayeb-Ali <Mr.elteb@gmail.com>
2020-05-20 22:00:29 +0100

    [fix] $with method lopp and time oute

--- app/Models/AcceptRequestSpecialists.php
+++ app/Models/AcceptRequestSpecialists.php
@@ -51,7 +51,7 @@ class AcceptRequestSpecialists extends Model
     use SoftDeletes;
 
     public $table = 'accept_request_specialists';
-    public $with = ['doctor', 'request'];
+//    public $with = ['doctor', 'request'];
 
 
     protected $dates = ['deleted_at'];
