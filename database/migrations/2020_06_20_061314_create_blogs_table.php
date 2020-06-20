1d35e682d9934a13ff4326c38b42df83c9055324
Tayeb-Ali <Mr.elteb@gmail.com>
2020-06-20 05:26:33 +0100

    [new] add blog migration #7

---
+++ database/migrations/2020_06_20_061314_create_blogs_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBlogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blogs', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->string('image')->nullable();
+            $table->text('content');
+            $table->boolean('notification')->default(true);
+            $table->boolean('status')->default(true);
+
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('emergency_requests');
+    }
+}
